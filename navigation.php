58efccbb4b8a16867fde3e84aef3bd435bc9faba
Chelsea Christie <cjohns48@lesley.edu>
2022-04-11 17:57:04 -0400

    added include nav php

---
+++ navigation.php
@@ -0,0 +1,10 @@
+
+
+<nav>
+    <div id="navigation">
+    <a href="index.php">HOME</a>
+    <a href="post.php">BLOGS</a>
+    <a href="post_submission.php">NEW BLOG POSTS</a>
+</div>
+</nav>
+</div>
