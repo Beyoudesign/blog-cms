ea0597f182a080df2935314daca6cd9e531a4f75
Chelsea Christie <cjohns48@lesley.edu>
2022-04-11 17:56:19 -0400

    created nav page

--- index.php
+++ index.php
@@ -2,14 +2,7 @@
     include "header.php";
 ?>
     
-<nav>
-    <div id="navigation">
-    <a href="index.php">HOME</a>
-    <a href="post.php">BLOGS</a>
-    <a href="post_submission.php">NEW BLOG POSTS</a>
-</div>
-</nav>
-</div>
+
     <main>
         
         <ul>
