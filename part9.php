9cd19ecf1aef1aec4b79efddb67f9100d92db540
Chelsea Christie <cjohns48@lesley.edu>
2022-04-11 18:13:17 -0400

    added all variables

--- part9.php
+++ part9.php
@@ -1,6 +1,16 @@
 <?php
     ini_set('display_errors', 1);  // show errors for debugging
+
+    $firstName = "Jane";  // string type variable
+    $lastName = "Doe"; // string type variable
+    $age = 35;  //integer type variable
+    $married = true:  //boolean type variable
+    $childrenNames = array("Alice", "Bob");  //array type variable
+    
+
+
+
 ?>
 
-$firstName = "Jane";  // string type variable
+
 
