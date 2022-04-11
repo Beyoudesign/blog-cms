2fc52f5621ec89834542f7eada2f195cdd92eca6
Chelsea Christie <cjohns48@lesley.edu>
2022-04-10 21:53:04 -0400

    resaved php files into htdocs folder

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+
+<title>
+    Chelsea Johnson
+</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+    <div id="header">
+        <h1>Chelsea's Bartending Blog</h1>
+    
+<nav>
+    <div id="navigation">
+    <a href="index.php">HOME</a>
+    <a href="post.php">BLOGS</a>
+    <a href="post_submission.php">NEW BLOG POSTS</a>
+</div>
+</nav>
+</div>
+    <main>
+        
+        <ul>
+            <li><a href="post.php">The Art of Mixology</a></li>
+            <li><a href="post.php">Perfecting your Counts</a></li>
+            <li><a href="post.php">Balance is Key</a></li>
+            
+        
+        </ul>
+    
+
+    </main>
+
+    <div id="footer">
+        Site built using Apache, PHP, MySQL, VSC, HTML, CSS by beyoudesign
+    </div>
+</body>
+</html>
