cba1b8f8c9d99a6f76ec9bc0bbeee7353a66465c
Your Name <you@example.com>
2022-09-27 22:03:34 -0400

    changed name again

--- dbh.ink.php
+++ dbh.ink.php
@@ -3,7 +3,7 @@
 $dbServer="localhost";
 $dbUsername="root"; //This is the same username as phpMyAdmin
 $dbPassword="root"; //This is the same password as phpMyAdmin
-$dbName="BLOG-CMS-1";
+$dbName="blog-cms-1";
 
 //Create connection
 $conn=mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
