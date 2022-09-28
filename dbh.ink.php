93c3be1853274adc60c5ad337885053508ee42d1
Your Name <you@example.com>
2022-09-27 21:54:17 -0400

    saved to blog

---
+++ dbh.ink.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbServer="localhost";
+$dbUsername="root"; //This is the same username as phpMyAdmin
+$dbPassword="root"; //This is the same password as phpMyAdmin
+$dbName="blog_cms";
+
+//Create connection
+$conn=mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+?>
+
+<?php include_once 'db_connect.php'?>
