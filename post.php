56f8dc917ee6319ebb65c3d587689c72107451f2
Your Name <you@example.com>
2022-09-27 21:40:19 -0400

    getting post details from database

--- post.php
+++ post.php
@@ -42,6 +42,21 @@
 ?>
 
 <?php
+    function getPostDetailsFromDatabase(){
+        //Get the post title
+        $postTitle = rawurldecode($_GET"The Art of Mixology");
+
+        //Get the post that matches the postTitle
+        include_once'db_connect.php';
+        $sql = "SELECT * FROM posts WHERE title='" .$postTitle ."'";
+        $result = mysqli_query($conn,$sql);
+
+        //Get the first row from the result as an associative array
+        $postDetails = mysqli_fetch_assoc($result);
+        return $postDetails;
+    }
+
+<?php
     // Post details contains all the data to generate the blog from
     $postDetails = getPostDetailsFromDatabase();
 ?>
