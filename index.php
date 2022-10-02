a1b0f35e0c57af56b7aa0812f9759ce649babbd8
beyoudesign <cjohns48@lesley.edu>
2022-10-02 12:40:47 -0400

    redoing get post details from database

--- index.php
+++ index.php
@@ -32,18 +32,17 @@
     
 <?php
     function getPostTitlesFromDatabase() {
-        // Get all the post titles from the posts table
+        // Get the post title
+        $postTitle = rawurldecode($_GET["title"]);
+        
+        //Get the post that matches the postTitle
         include_once 'includes2/dbh.inc.php';
-        $sql = "SELECT title FROM posts";
+        $sql = "SELECT * FROM posts WHERE title='" .$postTitle."'";
         $result = mysqli_query($conn, $sql);
 
-        // Get each result row as an assoc array, then add title to $postTitles
-        $postTitles = array("The Art of Mixology", "Perfecting your Counts", "Balance is Key");
-        while($row = mysqli_fetch_assoc($result)){
-            array_push($postTitles, $row['title']);
-        } 
-        return $postTitles;
-       
+        //Get the first row from the result as an associative array
+        $postDetails = mysqli_fetch_assoc($result);
+        return $postDetails;
     }
 
 
